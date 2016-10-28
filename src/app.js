100ab70888b6858727c389605acac51c313aafd4
Raquel Moss <raquelxmoss@gmail.com>
2016-10-29 11:35:49 +1300

    Render a beautiful Pythagoras tree

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
-import {div, pre, button} from '@cycle/dom';
+import {h, div, pre, button} from '@cycle/dom';
 import xs from 'xstream';
 
 const lSystem = require('./l-system');
+import Vector from './vector';
 
 const reducers = {
   GO (state, payload) {
@@ -9,9 +10,9 @@ const reducers = {
       ...state,
 
       system: lSystem(state.system, state.rules)
-    }
+    };
   }
-}
+};
 
 function App (sources) {
   const initialState = {
@@ -27,7 +28,7 @@ function App (sources) {
     .DOM
     .select('.go')
     .events('click')
-    .mapTo({type: 'GO'})
+    .mapTo({type: 'GO'});
 
   const action$ = xs.merge(
     goAction$
@@ -52,8 +53,111 @@ function view (state) {
   return div([
     button('.go', 'GO!'),
 
-    debug(state)
+    debug(state),
+
+    renderSystem(state)
   ]);
+}
+
+function turtleLine (start, end) {
+  return (
+    h(
+      'line',
+      {
+        attrs: {
+          x1: start.x,
+          y1: start.y,
+
+          x2: end.x,
+          y2: end.y,
+
+          stroke: 'black'
+        }
+      }
+    )
+  );
+}
+
+const LINE_LENGTH = 15;
+
+function project (origin, degrees, length) {
+  const angle = degrees * (Math.PI / 180);
+
+  return Vector({
+    x: origin.x * Math.cos(angle) - origin.y * Math.sin(angle),
+    y: origin.x * Math.sin(angle) + origin.y * Math.cos(angle)
+  });
+}
+
+function turtleItUp (turtleState, character, index, characters) {
+  if (character === '0' || character === '1') {
+    const lineEndPosition = turtleState.position.plus(
+      project(
+        {
+          x: LINE_LENGTH / Math.log(characters.length / 2),
+          y: 0
+        },
+        turtleState.direction
+      )
+    );
+
+    return {
+      ...turtleState,
+
+      position: lineEndPosition,
+
+      lines: [...turtleState.lines, turtleLine(turtleState.position, lineEndPosition)]
+    };
+  }
+
+  if (character === '[') {
+    return {
+      ...turtleState,
+
+      positions: [
+        ...turtleState.positions,
+
+        {position: turtleState.position, direction: turtleState.direction}
+      ],
+
+      direction: turtleState.direction + 45
+    };
+  }
+
+  if (character === ']') {
+    const poppedPosition = turtleState.positions[turtleState.positions.length - 1];
+
+    return {
+      ...turtleState,
+
+      position: poppedPosition.position,
+      direction: poppedPosition.direction - 45,
+
+      positions: turtleState.positions.slice(0, -1)
+    };
+  }
+}
+
+function renderSystem (state) {
+  const turtleState = {
+    position: Vector({
+      x: innerWidth / 2,
+      y: 600
+    }),
+
+    direction: 270,
+
+    positions: [
+    ],
+
+    lines: []
+  };
+
+  return (
+    h('svg', {attrs: {width: innerWidth, height: '100vh'}}, [
+      ...state.system.split('').reduce(turtleItUp, turtleState).lines
+    ])
+  );
 }
 
 function debug (val) {
